e965114d9f0d4ee6c38688ca1c337cb85dff5d00
Artem Pavlov <Zireaeell@yandex.ru>
2022-05-26 22:27:39 +0300

    Настройка стилей для ссылок при присутсвтии на определённой странице

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,19 @@
 import {useEffect, useState} from 'react';
-import {Link, NavLink} from 'react-router-dom';
+import {Link, NavLink, useLocation} from 'react-router-dom';
 
 import './Navigation.css';
 
 const Navigation = () => {
     const [showItems, setShowItems] = useState(false);
+    const {pathname} = useLocation();
 
     const handleToggleMenu = () => setShowItems(!showItems);
 
     useEffect(() => {
         setShowItems(false);
     }, []);
+
+    console.log(pathname)
 
     return (
         <nav className="navigation">
@@ -24,12 +27,15 @@ const Navigation = () => {
                                 <Link to="/" className="navigation__link">Главная</Link>
                             </li>
                             <li className="navigation__list-item">
-                                <NavLink to="/movies" className="navigation__link"
+                                <NavLink to="/movies"
+                                         className={`navigation__link ${pathname === '/movies' ? 'navigation__link_active' : ''}`}
                                          activeClassName="navigation__link_active">Фильмы</NavLink>
                             </li>
                             <li className="navigation__list-item">
-                                <NavLink to="/saved-movies" className="navigation__link"
-                                         activeClassName="navigation__link_active">Сохранённые фильмы</NavLink>
+                                <NavLink to="/saved-movies"
+                                         className={`navigation__link ${pathname === '/saved-movies' ? 'navigation__link_active' : ''}`}
+                                         activeClassName="navigation__link_active">Сохранённые
+                                    фильмы</NavLink>
                             </li>
                         </ul>
                     </div>
